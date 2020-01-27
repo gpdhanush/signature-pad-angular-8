4dfe59db926ccd69c1879f4a8e0d40e993d846eb
gpdhanush <agprakash406@gmail.com>
2020-01-27 15:23:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-signature-pad-8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-signature-pad-8)
---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, ViewChild } from '@angular/core';
+import { SignaturePad } from 'angular2-signaturepad/signature-pad';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements AfterViewInit {
+  @ViewChild(SignaturePad, {static: true}) signaturePad: SignaturePad;
+  signaturePadOptions: Object = {
+    'minWidth': 5,
+    'canvasWidth': 500,
+    'canvasHeight': 300
+  };
+ 
+  constructor() {}
+ 
+  ngAfterViewInit() {
+    this.signaturePad.set('minWidth', 5);
+    this.signaturePad.clear();
+  }
+  drawComplete() {
+    console.log(this.signaturePad.toDataURL());
+  }
+  drawStart() {
+    console.log('begin drawing');
+  }
+}
